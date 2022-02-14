046c2e126c6ecb92b4c680581f422f7c8b0f55b9
Ammar <mohammadammarkhan2@gmail.com>
2022-02-14 19:14:44 +0500

    Ajax Commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,14 +4,20 @@ class CommentsController < ApplicationController
   http_basic_authenticate_with name: 'ammar', password: 'khan', only: :destroy
 
   def create
-    @article = Article.find(params[:id])
-    @comments = @articl.comments.create(comment_params)
-    redirect_to article_path(@article)
+    @article = Article.find(params[:article_id])
+    @comments = @article.comments.create(comments_params)
+    respond_to do |format|
+      if @comments.save
+        redirect_to article_path(@article)
+
+        format.js
+      end
+    end
   end
 
   def destroy
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(params[:id])
+    @comment = @article.comments.create(params[:article_comment_id])
     @comment.destroy
     redirect_to article_path(@article), status: 303
   end
@@ -20,6 +26,6 @@ class CommentsController < ApplicationController
   private
 
   def comments_params
-    params.require(:comment).permits(:commenter, :body, :status)
+    params.require(:comment).permit(:commenter, :body, :status)
   end
 end
