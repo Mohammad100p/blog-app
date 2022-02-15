a4ae49a8644e9690dc3b0b112a49b35f513eda72
Ammar <mohammadammarkhan2@gmail.com>
2022-02-15 19:29:58 +0500

    Association Commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
   include Visible
 
   has_many :comments, -> { offset(1) }, dependent: :destroy
-
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10}
+  validates_associated :comments
+  validates :title, presence: true, allow_blank: false
+  validates :body, presence: true, length: { minimum: 10, too_short: " %{count} characters is the minimum allowed" }, format: { with: /\A[a-zA-Z]+\z/,
+  message: "only allows letters" }
 end
